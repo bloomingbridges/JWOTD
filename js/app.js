629ff55e30c94e036fde69bcfc92739094b61f6c
bloomingbridges <brueckner.florian@googlemail.com>
2014-04-17 11:50:43 +0200

    Version 1.1

--- js/app.js
+++ js/app.js
@@ -1,40 +1,80 @@
 (function() {
-  var $word_of_the_day, Word, bindUIHandlers, fetchLatestWord, isUpToDate, setWordTo;
+  var Word, bindUIHandlers, isUpToDate;
 
-  Word = (function() {
-    function Word() {
-      this.date = new Date();
-    }
-
-    Word.prototype.parse = function(item) {
+  Word = {
+    parse: function(item, isCached) {
+      var p, _i, _len, _ref, _results;
       this.kana = item.title;
-      this.date = new Date(item.pubDate);
-      this.kanji = /kanji:\s<a.*>(.*)<\/a>/gi.exec(item.description)[1];
-      this.romaji = /romaji: (.*)</.exec(item.description)[1];
-      return this.meaning = /definition: (.*)</gi.exec(item.description)[1];
-    };
-
-    Word.prototype.store = function() {
+      if (!isCached) {
+        this.date = new Date(item.pubDate);
+        this.kanji = /kanji:\s<a.*>(.*)<\/a>/gi.exec(item.description)[1];
+        this.romaji = /romaji: (.*)</.exec(item.description)[1];
+        return this.meaning = /definition: (.*)</gi.exec(item.description)[1];
+      } else {
+        _ref = ["date", "kanji", "romaji", "meaning"];
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          p = _ref[_i];
+          _results.push(this[p] = item[p]);
+        }
+        return _results;
+      }
+    },
+    fetch: function() {
+      var request;
+      if (navigator.onLine) {
+        console.log("CHECKING FOR NEW WORD..");
+        request = $.getJSON("http://pipes.yahoo.com/pipes/pipe.run?_id=8b43c55269d587214112bc421c1e4711&_render=json&_callback=?");
+        request.success((function(_this) {
+          return function(data) {
+            console.log("INCOMING WORD:", data.value.items[0].title);
+            _this.parse(data.value.items[0], false);
+            _this.store();
+            return _this.display();
+          };
+        })(this));
+        return request.error(function() {
+          return console.log("### ERROR - COULD NOT ACCESS FEED");
+        });
+      } else {
+        return console.log("### OFFLINE MODE");
+      }
+    },
+    store: function() {
       localStorage.setItem('lastUpdate', this.date);
-      return localStorage.setItem('word', JSON.stringify(this));
-    };
-
-    return Word;
-
-  })();
-
-  $word_of_the_day = new Word();
+      return localStorage.setItem('word', JSON.stringify({
+        kana: this.kana,
+        date: this.date,
+        kanji: this.kanji,
+        romaji: this.romaji,
+        meaning: this.meaning
+      }));
+    },
+    display: function() {
+      var p, _i, _len, _ref, _results;
+      _ref = ["kanji", "kana", "romaji", "meaning"];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        p = _ref[_i];
+        _results.push($("#" + p).html(this[p]));
+      }
+      return _results;
+    }
+  };
 
   $(function() {
+    var cached;
+    Word.date = new Date;
     if (localStorage.getItem('word')) {
-      $word_of_the_day = JSON.parse(localStorage.getItem('word'));
-      console.log("EXISTING: " + $word_of_the_day.meaning + " - " + $word_of_the_day.date);
-      setWordTo($word_of_the_day);
-      if (!isUpToDate(new Date($word_of_the_day.date), new Date())) {
-        fetchLatestWord();
+      cached = JSON.parse(localStorage.getItem('word'));
+      Word.parse(cached, true);
+      console.log("EXISTING: " + Word.meaning + " - " + Word.date);
+      Word.display();
+      if (!isUpToDate(new Date(Word.date), new Date())) {
+        Word.fetch();
       }
     } else {
-      fetchLatestWord();
+      Word.fetch();
     }
     bindUIHandlers();
     return $('#container').addClass('woosh');
@@ -61,31 +101,6 @@
 
   isUpToDate = function(lastPubDate, currentDate) {
     return lastPubDate.getDate() === currentDate.getDate();
-  };
-
-  fetchLatestWord = function() {
-    var request;
-    if (navigator.onLine) {
-      console.log("CHECKING FOR NEW WORD..");
-      request = $.getJSON("http://pipes.yahoo.com/pipes/pipe.run?_id=8b43c55269d587214112bc421c1e4711&_render=json&_callback=?");
-      request.success(function(data) {
-        $word_of_the_day.parse(data.value.items[0]);
-        $word_of_the_day.store();
-        return setWordTo($word_of_the_day);
-      });
-      return request.error(function() {
-        return console.log("### ERROR - COULD NOT ACCESS FEED");
-      });
-    } else {
-      return console.log("### OFFLINE MODE");
-    }
-  };
-
-  setWordTo = function(wotd) {
-    $('#kanji').html(wotd.kanji);
-    $('#kana').html(wotd.kana);
-    $('#romaji').html(wotd.romaji);
-    return $('#meaning').html(wotd.meaning);
   };
 
 }).call(this);
